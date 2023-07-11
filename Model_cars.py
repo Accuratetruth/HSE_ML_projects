ea1f86d7bad722e558c5ec95844bdb29bb472380
Polina <93820694+Accuratetruth@users.noreply.github.com>
2023-07-11 20:32:25 +0300

    Second change

--- Model_cars.py
+++ Model_cars.py
@@ -7,39 +7,39 @@ import sklearn
 def split_data(df: pd.DataFrame):
     y = df['selling_price']
     X = df[["max_power", "year", "torque", "km_driven", "mileage", "engine", "transmission",
-            "seats", "seller_type", "owner", "fuel"]]
+            "seats", "seller_type"]]
 
     return X, y
 
 def open_data(path="cars_dataset.csv"):
     df = pd.read_csv(path)
     df = df[['selling_price', "max_power", "year", "torque", "km_driven", "mileage", "engine", "transmission",
-            "seats", "seller_type", "owner","fuel"]]
+            "seats", "seller_type"]]
 
     return df
 
-def preprocess_data(df: pd.DataFrame, test=True):
-    df.dropna(inplace=True)
+# def preprocess_data(df: pd.DataFrame, test=True):
+#     df.dropna(inplace=True)
 
-    if test:
-        X_df, y_df = split_data(df)
-    else:
-        X_df = df
+#     if test:
+#         X_df, y_df = split_data(df)
+#     else:
+#         X_df = df
 
-    to_encode = ['owner', 'fuel']
-    for col in to_encode:
-        dummy = pd.get_dummies(X_df[col], prefix=col)
-        X_df = pd.concat([X_df, dummy], axis=1)
-        X_df.drop(col, axis=1, inplace=True)
+#     to_encode = ['owner', 'fuel']
+#     for col in to_encode:
+#         dummy = pd.get_dummies(X_df[col], prefix=col)
+#         X_df = pd.concat([X_df, dummy], axis=1)
+#         X_df.drop(col, axis=1, inplace=True)
 
-    if test:
-        return X_df, y_df
-    else:
-        return X_df
+#     if test:
+#         return X_df, y_df
+#     else:
+#         return X_df
 
 
 
-def load_model_and_predict(df, path="rf_model.pickle"):
+def load_model_and_predict(df, path="rf_model1.pickle"):
     with open(path, "rb") as file:
         model = load(file)
 
