d680d52d9303181e1270b81bb1eb4fed203a2095
Polina <93820694+Accuratetruth@users.noreply.github.com>
2023-07-20 19:03:16 +0300

    Model_cars.py

--- Model_cars.py
+++ Model_cars.py
@@ -18,40 +18,10 @@ def open_data(path="cars_dataset.csv"):
 
     return df
 
-# def preprocess_data(df: pd.DataFrame, test=True):
-#     df.dropna(inplace=True)
-
-#     if test:
-#         X_df, y_df = split_data(df)
-#     else:
-#         X_df = df
-
-#     to_encode = ['owner', 'fuel']
-#     for col in to_encode:
-#         dummy = pd.get_dummies(X_df[col], prefix=col)
-#         X_df = pd.concat([X_df, dummy], axis=1)
-#         X_df.drop(col, axis=1, inplace=True)
-
-#     if test:
-#         return X_df, y_df
-#     else:
-#         return X_df
-
-
-
 def load_model_and_predict(df, path="rf_model1.pickle"):
     with open(path, "rb") as file:
         model = load(file)
 
     prediction = model.predict(df)[0]
-    # prediction = np.squeeze(prediction)
-
-    #
-    # encode_prediction = {
-    #     0: "Сожалеем, вам не повезло",
-    #     1: "Ура! Вы будете жить"
-    # }
-    #
-    # prediction = encode_prediction[prediction]
-
+    
     return prediction
